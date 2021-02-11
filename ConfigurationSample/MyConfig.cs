d2615724eb781799fae18d021e9a881338d0e703
Christian Kitte <ckitte@produktundmarkt.de>
2021-02-11 15:39:12 +0100

    Projektdateien hinzufügen.

---
+++ ConfigurationSample/MyConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationSample
+{
+    public class MyConfig
+    {
+        public string ApplicationName { get; set; }
+        public int Version { get; set; }
+    }
+}
---
+++ ConfigurationSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationSample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, service) =>
+                {
+                    service.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<StartUp>();
+                });
+    }
+
+    class StartUp : IHostedService
+    {
+        public StartUp(IConfiguration test)
+        {
+            IConfiguration _test = test;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(test.GetSection("MyConfig")["ApplicationName"]);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
